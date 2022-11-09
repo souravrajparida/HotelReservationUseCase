e7927f4e3ff84c1a8809c801d6f8c2c399ec18a4
Souravraj Parida <51021938+souravrajparida@users.noreply.github.com>
2022-11-09 12:52:36 +0530

    Delete HotelAngular/src directory

--- HotelAngular/src/app/models/user-reg.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserRegistration } from './user-reg';
-
-describe('UserRegistration', () => {
-  it('should create an instance', () => {
-    expect(new UserRegistration(0,"","","","","","")).toBeTruthy();
-  });
-});
--- HotelAngular/src/app/models/user-reg.ts
+++
@@ -1,5 +0,0 @@
-export class UserRegistration {
-constructor (public customerId: number, public emailAddress: string, public password: string,
-    public firstName: string, public lastName: string, public phoneNumber: string,
-    public address: string) { }
-}
